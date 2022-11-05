c39633d84909fbf27ffeca9175a4fb691d379ec8
0xtaofeek <taofeek01@yahoo.com>
2022-11-05 14:09:39 +0000

    refactor for test

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,8 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image ==="" ? "/placeholder.png" : item.image}
+        src={item.image}
+        onError={()=>this.src="/placeholder.png"}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
